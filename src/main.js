98fc34c51f02fd83b5f0e90d0b996a1e047fc336
Aleandro Presta <37838171+AleandroPresta@users.noreply.github.com>
2024-12-29 12:16:44 +0100

    Refactor password input styles and restructure HTML for improved organization

--- src/main.js
+++ src/main.js
@@ -1,8 +1,6 @@
 import Menu from './menu/menu.js';
-import keypad from './password/password.js';
 
 document.addEventListener('DOMContentLoaded', async () => {
-    await keypad.waitForCorrectPassword();
     const appContainer = document.getElementById('app');
     const menu = new Menu(appContainer);
     await menu.initialize();
