0fc2eb5f422e3a5bf56da6bf5a21aac9fa072357
Aleandro Presta <37838171+AleandroPresta@users.noreply.github.com>
2024-12-29 14:45:45 +0100

    Add loading component with progress bar and integrate into main application flow

--- src/main.js
+++ src/main.js
@@ -1,11 +1,24 @@
 import Menu from './menu/menu.js';
 import PasswordKeypad from './password/password.js';
+import LoadingComponent from './loading/loading.js';
 
-let componentToShow = 'password';
+let componentToShow = 'loading';
 let successSound = document.getElementById('success-sound');
 
 document.addEventListener('DOMContentLoaded', async () => {
     const appContainer = document.getElementById('app');
+
+    if (componentToShow === 'loading') {
+        const loading = new LoadingComponent(appContainer);
+        await loading.initialize();
+        // Listen for the custom event
+        document.addEventListener('loadingFinished', () => {
+            console.log("Loading finished");
+            componentToShow = 'password';
+            const keypad = new PasswordKeypad(appContainer);
+            keypad.initialize();
+        });
+    }
 
     if (componentToShow === 'password') {
         const keypad = new PasswordKeypad(appContainer);
